0b3bda1867a709cd8d78b1018d0856ef549f7980
yuxuan-bill <luyxapp@163.com>
2019-02-09 00:03:20 -0800

    working version of ContentFinder except for content getting part

--- ContentFinder.py
+++ ContentFinder.py
@@ -3,28 +3,43 @@
 # 1. the source of article,
 # 2. its date, time,
 # 3. title,
-# 4. and content.
+# 4. url,
+# 5. and content.
 
 from bs4 import BeautifulSoup
 import json
+import urllib.request
+import ssl
 
-HTML_DOC = open("test.html", 'r', encoding = 'utf-8')
+# resolve certificate verify failed error,
+# see: https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
+context = ssl._create_unverified_context()
+
+HTML_DOC = open("test.html", 'r', encoding='utf-8')
 
 soup = BeautifulSoup(HTML_DOC, 'html.parser')
 entries = soup.findAll('tr', {'class': 'headline'})
 
-# list of dictionary, e.g. [{url: --, date: --, time: --, title: --, content: --}, {...}, ...]
+# list of dictionary, e.g. [{url: --, source: --, date: --, time: --, title: --, content: --}, {...}, ...]
 articleInfo = []
 for entry in entries:
     simplifiedEntry = entry.find_all('td')
     # contains headline, date'n'time
-    entry1 = simplifiedEntry[0].find('input')['data-metrics-data']
+    entry1 = json.loads(simplifiedEntry[0].find('input')['data-metrics-data'])
     # contains url to content
-    entry2 = simplifiedEntry[2].find('a')
+    entry2 = simplifiedEntry[2]
 
-    print(entry1)
-    print(entry2)
-    print()
+    title = entry1['Hdl']
+    (time, date) = entry1['Pd'].split(", ")
+    url = entry2.find('a')['href'].replace('..', 'https://global.factiva.com')
+    source = entry2.find(class_='leadFields').find('a').text
+    # html file that contains the article content
+    # TODO: may fail parsing in case this article is only available on some third party website
+    print(url)
+    contentHtml = urllib.request.urlopen(url, context=context).read().decode('utf8')
+    contentSoup = BeautifulSoup(contentHtml, 'html.parser') # TODO: get intermediate page instead of final result
+    print(contentSoup)
+    print("--------------------")
 
 
     #simplifiedEntry = list(BeautifulSoup(entry, 'html.parser').findAll('td')[i] for i in [0, 2])
