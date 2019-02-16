999a500459bf5ae7c74085d9817c732863d82e95
yuxuan-bill <luyxapp@163.com>
2019-02-16 00:37:19 -0800

    Implement contentFinder

--- ContentFinder.py
+++ ContentFinder.py
@@ -1,47 +1,22 @@
-
-# From a Factiva search result page, get the following information for each of the articles:
-# 1. the source of article,
-# 2. its date, time,
+# From a Factiva article page, get the following information about the article:
+# 1. its date,
+# 2. time,
 # 3. title,
-# 4. url,
-# 5. and content.
+# 4. and content.
 
 from bs4 import BeautifulSoup
-import json
-import urllib.request
-import ssl
-
-# resolve certificate verify failed error,
-# see: https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
-context = ssl._create_unverified_context()
-
-HTML_DOC = open("test.html", 'r', encoding='utf-8')
-
-soup = BeautifulSoup(HTML_DOC, 'html.parser')
-entries = soup.findAll('tr', {'class': 'headline'})
-
-# list of dictionary, e.g. [{url: --, source: --, date: --, time: --, title: --, content: --}, {...}, ...]
-articleInfo = []
-for entry in entries:
-    simplifiedEntry = entry.find_all('td')
-    # contains headline, date'n'time
-    entry1 = json.loads(simplifiedEntry[0].find('input')['data-metrics-data'])
-    # contains url to content
-    entry2 = simplifiedEntry[2]
-
-    title = entry1['Hdl']
-    (time, date) = entry1['Pd'].split(", ")
-    url = entry2.find('a')['href'].replace('..', 'https://global.factiva.com')
-    source = entry2.find(class_='leadFields').find('a').text
-    # html file that contains the article content
-    # TODO: may fail parsing in case this article is only available on some third party website
-    print(url)
-    contentHtml = urllib.request.urlopen(url, context=context).read().decode('utf8')
-    contentSoup = BeautifulSoup(contentHtml, 'html.parser') # TODO: get intermediate page instead of final result
-    print(contentSoup)
-    print("--------------------")
 
 
-    #simplifiedEntry = list(BeautifulSoup(entry, 'html.parser').findAll('td')[i] for i in [0, 2])
-    #print(simplifiedEntry)
-    #print()
+# html: a string that stores the page source code
+# return: a dictionary of content information about the article, in the form
+#       {source: --, date: --, time: --, title: --, content: [--, --, ...]}
+def getContent(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    article_container = soup.find("div", {"class": "article enArticle"})
+    header = article_container.findAll("div", {"class": None})
+    article_info = {}
+    article_info["title"] = header[1].find("span").string
+    article_info["date"] = header[3].string
+    article_info["time"] = header[4].string
+    article_info["content"] = list(paragraph.getText() for paragraph in article_container.findAll("p", {"class": "articleParagraph enarticleParagraph"}))
+    return article_info
--- example.py
+++ example.py
@@ -1,4 +1,16 @@
-from selenium import webdriver
-browser = webdriver.Chrome('/Users/luyuxuan/Desktop/scrape/chromedriver')
-browser.get('http://guides.lib.uw.edu/factiva')
+# from selenium import webdriver
+# browser = webdriver.Chrome('/Users/luyuxuan/Desktop/scrape/chromedriver')
+# html = browser.get('http://guides.lib.uw.edu/factiva')
+# print(html.page_source)
+# browser.get('https://global.factiva.com/du/article.aspx/?accessionno=RSTPROPE20181025eeap001b9&fcpil=en&napc=S&sa_from=&cat=a')
 
+
+from selenium import webdriver
+
+
+driver = webdriver.Chrome('/Users/luyuxuan/Desktop/scrape/chromedriver')
+driver.implicitly_wait(10)
+driver.get('https://global.factiva.com/du/article.aspx/?accessionno=RSTPROPE20181025eeap001b9&fcpil=en&napc=S&sa_from=&cat=a')
+
+html = driver.page_source
+print(html)
